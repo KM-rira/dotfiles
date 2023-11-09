b939adcced698fac6678ae8a269031c991c65c0a
koji <zamel1205@gmail.com>
2023-11-09 16:40:31 +0000

    add rebase helper module

---
+++ tools/rebase_helper.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# 引数の数をチェック
+if [ "$#" -ne 1 ]; then
+    echo "Usage: $0 <number>"
+    exit 1
+fi
+
+# 引数が数字かどうかをチェック
+if ! [[ "$1" =~ ^[0-9]+$ ]]; then
+    echo "Error: Argument must be a number"
+    exit 1
+fi
+
+# git rebase -i コマンドを実行
+git rebase -i "HEAD~$1"
