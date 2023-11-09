7d70173641e1bbc74334ea7024b97b711ba190ac
koji <zamel1205@gmail.com>
2023-11-09 16:40:10 +0000

    add rebase command

--- addAlias.sh
+++ addAlias.sh
@@ -1,22 +1,24 @@
 #!/bin/bash
 
 # 複数のエイリアスを効率的に`.bashrc`に追加するためには、スクリプトを一般化して、追加したいエイリアスを簡単にリストに追加できるようにすると便利です。
-# 以下のスクリプトは、追加したいエイリアスを配列として列挙し、それらのエイリアスが`.bashrc`に存在しない場合にだけ追加します：
+# 以下のスクリプトは、追加したいエイリアスを配列として列挙し、それらのエイリアスが`.bashrc`に存在しない場合にだけ追加します。
 
 # bashrcに下記を追加。（非同期で）
-# ~/vimConf/addAlias.sh
+# nohup bash ~/vimConf/addAlias.s > /dev/null 2>&1 &h
+
+# bashrcに下記を追記すること。
+# unalias -a
+# source ~/vimConf/addAlias.sh
 
 # .bashrcのパス
 BASHRC="$HOME/.bashrc"
 
 # 追加したいエイリアスをこの配列に列挙
-# unalias -a
-# source addAlias.sh
-
 alias historyg='history | grep'
 alias open='nvim ~/.bashrc'
 alias ..='cd ..'
 alias ...='cd ../..'
+alias ....='cd ../../..'
 alias ll='ls -lt'
 alias re='source ~/.bashrc'
 alias bashrc='nv ~/.bashrc'
@@ -38,5 +40,9 @@ alias findall='sudo find / -name'
 alias e='exa -l'
 alias batfzf='fzf | xargs bat'
 alias nvfzf='fzf | xargs nvim'
+alias continue='git rebase --continue'
+alias abort='git rebase --abort'
+alias amend='git commit --amend -m'
+alias rebasei='bash ~/vimConf/tools/rebase-helper.sh'
 
 echo "Done updating alias"
