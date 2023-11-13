903579f980f9cc788dc3b14938fbf265d31e7929
koji <zamel1205@gmail.com>
2023-11-13 17:19:02 +0900

    fix fzf command

---
+++ tools/book_mark_fzf.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Bookmarkフォルダ内のファイルをfzfで検索
+selected_file=$(ls ~/bookmark | fzf)
+
+# ファイルが選択されなかった場合の処理
+if [ -z "$selected_file" ]; then
+    echo "ファイルが選択されませんでした。"
+    exit 1
+fi
+
+# 選択されたファイルからURLを取得
+url=$(cat ~/bookmark/"$selected_file")
+
+# URLをブラウザで開く
+start "$url"
