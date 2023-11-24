ad5a3772c94baf209e64b272bfd2e6117920def7
koji <zamel1205@gmail.com>
2023-11-24 12:54:17 +0900

    add gh pr list filter

--- tools/prl.sh
+++ tools/prl.sh
@@ -6,4 +6,25 @@ selected_repo=$(gh repo list ${gh_user} | fzf)
 # 選択されたリポジトリからリポジトリ名を抜き取る
 repo_name=$(echo "$selected_repo" | awk '{print $1}')
 
-gh pr list --repo "$repo_name"
+# 第一引数を変数に格納
+option="$1"
+
+# case文を使用して引数に応じた処理を行う
+case $option in
+    a)
+        gh pr list --state all --repo "$repo_name"
+        ;;
+    o)
+        gh pr list --state open --repo "$repo_name"
+        ;;
+    c)
+        gh pr list --state closed --repo "$repo_name"
+        ;;
+    d)
+        gh pr list --draft --repo "$repo_name"
+        ;;
+    *)
+        gh pr list --repo "$repo_name"
+        ;;
+esac
+
