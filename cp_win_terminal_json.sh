37db289dacc2ebde2f263e7037f25a191884e529
koji <zamel1205@gmail.com>
2023-11-25 17:10:33 +0900

    update win terminal setting json

--- cp_win_terminal_json.sh
+++ cp_win_terminal_json.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-cp /c/Users/tomat/AppData/Local/Packages/Microsoft.WindowsTerminal_8wekyb3d8bbwe/LocalState/settings.json ~/vimConf/settings.json
+cp ${wt_path} "$HOME/vimConf/terminal_settings.json"
