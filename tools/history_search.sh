27dc00d23dc929cf57587642ada015583ad2601b
koji <zamel1205@gmail.com>
2023-11-27 23:05:05 +0900

    add history search module

---
+++ tools/history_search.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Chrome履歴ファイルのパス
+CHROME_HISTORY="$HOME/Library/Application Support/Google/Chrome/Default/History"
+
+# 履歴ファイルのコピーを作成（Chromeが使用中の場合のため）
+HISTORY_DB="$HOME/chrome_history_copy.db"
+cp "$CHROME_HISTORY" "$HISTORY_DB"
+
+# SQLiteコマンドを使用してURLのタイトルを抽出し、fzfでフィルタリング
+# 選択された行からURLを取得
+sqlite3 "$HISTORY_DB" "SELECT title, url FROM urls ORDER BY last_visit_time DESC" |
+    awk -F'|' '{print $1}' |
+    fzf --no-sort --reverse --prompt='Search History: ' --no-multi |
+    grep -Ff - "$HISTORY_DB" | awk -F'|' '{print $2}'
+
+# コピーした履歴ファイルを削除
+rm "$HISTORY_DB"
