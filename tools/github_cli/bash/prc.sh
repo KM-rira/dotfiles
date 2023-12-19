b13302e53b623e6ce02b5e6ca90b0578a7b6f1a3
koji <zamel1205@gmail.com>
2023-12-19 19:24:32 +0900

    fix repo url edit

--- tools/github_cli/bash/prc.sh
+++ tools/github_cli/bash/prc.sh
@@ -6,7 +6,8 @@ source ~/vimConf/tools/github_cli/bash/config.sh
 repo_url=$(git config --get remote.origin.url)
 
 # URLからリポジトリ名を抽出
-selected_repo=$(echo $repo_url | sed -e 's/\.git$//' -e 's/^https:\/\/github\.com\///' -e 's/^git@github\.com://' -e 's/:/\//')
+# selected_repo=$(echo $repo_url | sed -e 's/\.git$//' -e 's/^https:\/\/github\.com\///' -e 's/^git@github\.com://' -e 's/:/\//')
+selected_repo=$(echo $repo_url | sed -e 's/.*://' -e 's/\.git$//')
 
 # リポジトリ名の抽出失敗した場合
 if [ -z "$selected_repo" ]; then
