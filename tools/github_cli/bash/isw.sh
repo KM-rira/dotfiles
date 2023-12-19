4d48580c1216807f25123a5f2f12dead7dc577b9
koji <zamel1205@gmail.com>
2023-12-19 19:30:25 +0900

    other gh command update

--- tools/github_cli/bash/isw.sh
+++ tools/github_cli/bash/isw.sh
@@ -2,8 +2,17 @@
 
 source ~/vimConf/tools/github_cli/bash/config.sh
 
-# GitHubのリポジトリ一覧を取得し、fzfで選択させる
-selected_repo=$(gh repo list ${gh_user} --limit ${Limit1} | fzf)
+# Gitリポジトリのorigin URLを取得
+repo_url=$(git config --get remote.origin.url)
+
+# URLからリポジトリ名を抽出
+selected_repo=$(echo $repo_url | sed -e 's/.*://' -e 's/\.git$//')
+
+# リポジトリ名の抽出失敗した場合
+if [ -z "$selected_repo" ]; then
+    # GitHubのリポジトリ一覧を取得し、fzfで選択させる
+    selected_repo=$(gh repo list ${gh_user} --limit ${Limit1} | fzf)
+fi
 
 # 選択されたリポジトリからリポジトリ名を抜き取る
 repo_name=$(echo "$selected_repo" | awk '{print $1}')
