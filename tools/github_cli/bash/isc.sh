8f6202b9325fead41947b05da37f045060f6b87d
koji <zamel1205@gmail.com>
2023-12-19 17:56:46 +0900

    fix gh command

--- tools/github_cli/bash/isc.sh
+++ tools/github_cli/bash/isc.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
+source ~/vimConf/tools/github_cli/bash/config.sh
+
 # GitHubのリポジトリ一覧を取得し、fzfで選択させる
-selected_repo=$(gh repo list ${gh_user} | fzf)
+selected_repo=$(gh repo list ${gh_user} --limit ${Limit1} | fzf)
 
 # 選択されたリポジトリからリポジトリ名を抜き取る
 repo_name=$(echo "$selected_repo" | awk '{print $1}')
-
-source ~/vimConf/tools/github_cli/bash/config.sh
 
 # 引数の数をチェック
 case "$#" in
