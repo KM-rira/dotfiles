9925262190b3f4fb6ec988e75537debab3b99e35
koji <zamel1205@gmail.com>
2023-12-19 18:29:32 +0900

    fix equal

--- tools/github_cli/bash/get_repo.sh
+++ tools/github_cli/bash/get_repo.sh
@@ -12,7 +12,7 @@ if [ -z "$repo_url" ]; then
 fi
 
 # URLからリポジトリ名を抽出
-Selected_repo==$(echo $repo_url | sed -e 's/\.git$//' -e 's/^https:\/\/github\.com\///' -e 's/^git@github\.com://' -e 's/:/\//')
+Selected_repo=$(echo $repo_url | sed -e 's/\.git$//' -e 's/^https:\/\/github\.com\///' -e 's/^git@github\.com://' -e 's/:/\//')
 
 # リポジトリ名が取得できたかどうかを確認
 if [ -z "$Selected_repo" ]; then
