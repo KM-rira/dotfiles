c1280a278c844b3c91979ffe8ef8abab4409e09a
koji <zamel1205@gmail.com>
2023-12-19 19:16:19 +0900

    update prc

--- tools/github_cli/bash/prc.sh
+++ tools/github_cli/bash/prc.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
 
 source ~/vimConf/tools/github_cli/bash/config.sh
-source ~/vimConf/tools/github_cli/bash/get_repo.sh
 
-if [ -z "$Selected_repo" ]; then
+# Gitリポジトリのorigin URLを取得
+repo_url=$(git config --get remote.origin.url)
+
+# URLからリポジトリ名を抽出
+selected_repo=$(echo $repo_url | sed -e 's/\.git$//' -e 's/^https:\/\/github\.com\///' -e 's/^git@github\.com://' -e 's/:/\//')
+
+# リポジトリ名の抽出失敗した場合
+if [ -z "$selected_repo" ]; then
     # GitHubのリポジトリ一覧を取得し、fzfで選択させる
-    Selected_repo=$(gh repo list ${gh_user} --limit ${Limit1} | fzf)
+    selected_repo=$(gh repo list ${gh_user} --limit ${Limit1} | fzf)
 fi
 
 # 選択されたリポジトリからリポジトリ名を抜き取る
-repo_name=$(echo "$Selected_repo" | awk '{print $1}')
+repo_name=$(echo "$selected_repo" | awk '{print $1}')
 
 # 引数の数をチェック
 case "$#" in
