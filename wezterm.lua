5e57c90f90ca2e7dc764c9f59d014231b95f5930
koji <zamel1205@gmail.com>
2024-01-09 17:12:55 +0900

    wez command update

--- wezterm.lua
+++ wezterm.lua
@@ -23,40 +23,44 @@
         {key="Tab", mods="CTRL", action=wezterm.action{ActivateTabRelative=1}},
         -- 前のタブに移動
         {key="Tab", mods="CTRL|SHIFT", action=wezterm.action{ActivateTabRelative=-1}},
-
-        {key="v", mods="SUPER", action=wezterm.action{PasteFrom="Clipboard"}},
+        -- windowキー無効化
+        {key="V", mods="SUPER", action="Nop"},
         -- 1ページ上にスクロール
-        {key="PageUp", mods="ALT", action=wezterm.action{ScrollByPage=-1}},
+        {key="b", mods="ALT", action=wezterm.action{ScrollByPage=-1}},
         -- 1ページ下にスクロール
-        {key="PageDown", mods="ALT", action=wezterm.action{ScrollByPage=1}},
+        {key="f", mods="ALT", action=wezterm.action{ScrollByPage=1}},
+        -- 半ページを下にスクロール
+        {key="d", mods="ALT", action=wezterm.action{ScrollByPage=-0.5}},
+        -- 半ページを上にスクロール
+        {key="u", mods="ALT", action=wezterm.action{ScrollByPage=0.5}},
         -- 一行上にスクロール
         {key="k", mods="ALT", action=wezterm.action{ScrollByLine=-1}},
         -- 一行下にスクロール
         {key="j", mods="ALT", action=wezterm.action{ScrollByLine=1}},
         -- ペインを垂直分割
-        {key="v", mods="ALT", action=wezterm.action{SplitHorizontal={domain="CurrentPaneDomain"}}},
+        {key="v", mods="SHIFT|CTRL|ALT", action=wezterm.action{SplitHorizontal={domain="CurrentPaneDomain"}}},
         -- ペインを水平分割
-        {key="h", mods="ALT", action=wezterm.action{SplitVertical={domain="CurrentPaneDomain"}}},
+        {key="h", mods="SHIFT|CTRL|ALT", action=wezterm.action{SplitVertical={domain="CurrentPaneDomain"}}},
         -- タブを左に移動
         { key = '<', mods = 'SHIFT|ALT', action = wezterm.action.MoveTabRelative(-1) },
         -- タブを右に移動
         { key = '>', mods = 'SHIFT|ALT', action = wezterm.action.MoveTabRelative(1) },
         -- 次のペインへ移動
-        {key="RightArrow", mods="CTRL|ALT", action=wezterm.action{ActivatePaneDirection="Right"}},
+        {key="l", mods="CTRL|ALT", action=wezterm.action{ActivatePaneDirection="Right"}},
         -- 前のペインへ移動
-        {key="LeftArrow", mods="CTRL|ALT", action=wezterm.action{ActivatePaneDirection="Left"}},
+        {key="h", mods="CTRL|ALT", action=wezterm.action{ActivatePaneDirection="Left"}},
         -- 上下のペイン間を移動するための追加のショートカット
-        {key="DownArrow", mods="CTRL|ALT", action=wezterm.action{ActivatePaneDirection="Down"}},
-        {key="UpArrow", mods="CTRL|ALT", action=wezterm.action{ActivatePaneDirection="Up"}},
+        {key="j", mods="CTRL|ALT", action=wezterm.action{ActivatePaneDirection="Down"}},
+        {key="k", mods="CTRL|ALT", action=wezterm.action{ActivatePaneDirection="Up"}},
         -- 矢印キーを使用してペインのサイズを調整
-        {key="RightArrow", mods="SHIFT|ALT", action=wezterm.action{AdjustPaneSize={"Right", 1}}},
-        {key="LeftArrow", mods="SHIFT|ALT", action=wezterm.action{AdjustPaneSize={"Left", 1}}},
-        {key="UpArrow", mods="SHIFT|ALT", action=wezterm.action{AdjustPaneSize={"Up", 1}}},
-        {key="DownArrow", mods="SHIFT|ALT", action=wezterm.action{AdjustPaneSize={"Down", 1}}},
+        {key="l", mods="SHIFT|ALT", action=wezterm.action{AdjustPaneSize={"Right", 1}}},
+        {key="h", mods="SHIFT|ALT", action=wezterm.action{AdjustPaneSize={"Left", 1}}},
+        {key="k", mods="SHIFT|ALT", action=wezterm.action{AdjustPaneSize={"Up", 1}}},
+        {key="j", mods="SHIFT|ALT", action=wezterm.action{AdjustPaneSize={"Down", 1}}},
         -- アクティブなペインを閉じる
         {key="x", mods="ALT", action=wezterm.action{CloseCurrentPane={confirm=true}}},
     },
     audible_bell = "Disabled",audible_bell = "Disabled",
-    font_size = 11.0,
+    font_size = 9.0,
  }
 
