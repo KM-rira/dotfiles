6884e50c603f3162b41e9ed9956159a0988d0a01
koji <zamel1205@gmail.com>
2024-02-28 02:02:50 +0900

    remove error plugin

--- nvim/plug.lua
+++ nvim/plug.lua
@@ -40,14 +40,8 @@ require('packer').startup(function(use)
     use {'akinsho/bufferline.nvim', tag = '*'}
     use 'folke/tokyonight.nvim'
     use { "catppuccin/nvim", as = "catppuccin" }
-    use 'rcarriga/nvim-notify'
-    use {
-        'gelguy/wilder.nvim',
-        config = function()
-            -- config goes here
-        end,
-    }
-    use 'folke/noice.nvim'
+    --use 'rcarriga/nvim-notify'
+    --use 'folke/noice.nvim'
     use 'norcalli/nvim-colorizer.lua'
     use 'yamatsum/nvim-cursorline'
     use 'sindrets/diffview.nvim'
@@ -135,32 +129,26 @@ require('spectre.actions').get_state()
 
 require 'colorizer'.setup()
 
-local wilder = require('wilder')
-wilder.setup({modes = {':', '/', '?'}})
-wilder.set_option('renderer', wilder.popupmenu_renderer({
-  pumblend = 20,
-}))
+-- require("noice").setup({
+--   lsp = {
+--     -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+--     override = {
+--       ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+--       ["vim.lsp.util.stylize_markdown"] = true,
+--       ["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
+--     },
+--   },
+--   -- you can enable a preset for easier configuration
+--   presets = {
+--     bottom_search = true, -- use a classic bottom cmdline for search
+--     command_palette = true, -- position the cmdline and popupmenu together
+--     long_message_to_split = true, -- long messages will be sent to a split
+--     inc_rename = false, -- enables an input dialog for inc-rename.nvim
+--     lsp_doc_border = false, -- add a border to hover docs and signature help
+--   },
+-- })
 
-require("noice").setup({
-  lsp = {
-    -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
-    override = {
-      ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-      ["vim.lsp.util.stylize_markdown"] = true,
-      ["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
-    },
-  },
-  -- you can enable a preset for easier configuration
-  presets = {
-    bottom_search = true, -- use a classic bottom cmdline for search
-    command_palette = true, -- position the cmdline and popupmenu together
-    long_message_to_split = true, -- long messages will be sent to a split
-    inc_rename = false, -- enables an input dialog for inc-rename.nvim
-    lsp_doc_border = false, -- add a border to hover docs and signature help
-  },
-})
-
-require("notify").setup{}
+--require("notify").setup{}
 
 require("bufferline").setup{}
 
