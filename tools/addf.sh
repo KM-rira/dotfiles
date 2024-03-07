03ab5927d82324ae04897c0390aab0a42d5b5149
koji <zamel1205@gmail.com>
2024-03-07 23:49:14 +0900

    add new command

---
+++ tools/addf.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# コマンド履歴を取得してfzfでフィルタリング
+cd $(git rev-parse --show-toplevel)
+select_file=$( git diff --name-only | fzf --no-sort --prompt='SELECT ADD FILE: ' --no-multi)
+cd -
+
+# リポジトリ名の抽出失敗した場合
+if [ -z "$select_file" ] ; then
+    echo "===== exit process ====="
+    exit
+fi
+
+# 選択されたコマンドを実行
+git add $select_file
+
---
+++ tools/restoresf.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# コマンド履歴を取得してfzfでフィルタリング
+cd $(git rev-parse --show-toplevel)
+select_file=$( git diff --name-only --cached | fzf --no-sort --prompt='SELECT STAGING RESTORE FILE: ' --no-multi)
+cd -
+
+# リポジトリ名の抽出失敗した場合
+if [ -z "$select_file" ] ; then
+    echo "===== exit process ====="
+    exit
+fi
+
+# 選択されたコマンドを実行
+git add $select_file
