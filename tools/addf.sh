9d39358de4ad1c17138ee4c46ceb3e4c87d67477
koji <zamel1205@gmail.com>
2024-03-08 00:34:28 +0900

    option is multi

--- tools/addf.sh
+++ tools/addf.sh
@@ -2,7 +2,7 @@
 
 # コマンド履歴を取得してfzfでフィルタリング
 cd $(git rev-parse --show-toplevel)
-select_file=$( git diff --name-only | fzf --no-sort --prompt='SELECT ADD FILE: ' --no-multi)
+select_file=$( git diff --name-only | fzf --no-sort --prompt='SELECT ADD FILE: ' --multi)
 
 # リポジトリ名の抽出失敗した場合
 if [ -z "$select_file" ] ; then
