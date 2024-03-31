8bcc5d9f7b7afc929dfda6897c559b5532afd685
koji <zamel1205@gmail.com>
2024-03-31 14:59:32 +0900

    add cf command

---
+++ tools/cf.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# コマンド履歴を取得してfzfでフィルタリング
+select_file=$( ls -d */ | fzf --no-sort --prompt='CHANGE DIR: ')
+
+# リポジトリ名の抽出失敗した場合
+if [ -z "$select_file" ] ; then
+    echo "===== exit process ====="
+    exit
+fi
+
+# 選択されたコマンドを実行
+cd $select_file
+
+echo "===== done ====="
+
