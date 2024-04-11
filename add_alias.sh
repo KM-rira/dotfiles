8effb098a82137148c0db5efd30f40173645a54d
koji <zamel1205@gmail.com>
2024-04-11 15:17:23 +0900

    remove asterisk

--- add_alias.sh
+++ add_alias.sh
@@ -129,7 +129,7 @@ alias dcu='docker compose up --build -d'
 alias dlogsf='docker logs -f $(dockerf)'
 alias commitid='bash ~/vimConf/tools/get_commit_id.sh'
 alias d='bash ~/vimConf/tools/do.sh'
-alias ff="ls -F | grep -v / | fzf --height 70% --layout reverse --info inline --border     --preview 'batcat --color=always {}' --preview-window '~3'      --bind 'ctrl-/:change-preview-window(50%|hidden|)'     --color 'fg:#bbccdd,fg+:#ddeeff,bg:#334455,preview-bg:#223344,border:#778899'"
+alias ff="ls -F | grep -v / | sed 's/\*//g' | fzf --height 70% --layout reverse --info inline --border     --preview 'batcat --color=always {}' --preview-window '~3'      --bind 'ctrl-/:change-preview-window(50%|hidden|)'     --color 'fg:#bbccdd,fg+:#ddeeff,bg:#334455,preview-bg:#223344,border:#778899'"
 
 # gh
 alias gpl="bash ~/vimConf/tools/github_cli/bash/gpl.sh"
