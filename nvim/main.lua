8cf6e75b84e318df5b1e8c2882fd25e5f66fc0c1
koji <zamel1205@gmail.com>
2024-08-11 22:14:24 +0900

    vscode neovim plugin

--- nvim/main.lua
+++ nvim/main.lua
@@ -1,8 +1,15 @@
-require('plug')
-require('auto')
-require('command')
-require('func')
-require('keymap')
-require('setting')
-require('view')
+if vim.g.vscode then
+    -- VS Code用の設定
+    -- vim.cmd('source /path/to/vscode-specific-config.lua')
+    require('vscode_nvim')
+else
+    -- 通常のNeovim用の設定
+    require('plug')
+    require('auto')
+    require('command')
+    require('func')
+    require('keymap')
+    require('setting')
+    require('view')
+end
 
