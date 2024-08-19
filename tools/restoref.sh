f821c22ba225b0c00667fb9fb7259fada3cfe586
koji <zamel1205@gmail.com>
2024-08-19 18:08:23 +0900

    update restoref to add s option

--- tools/restoref.sh
+++ tools/restoref.sh
@@ -1,6 +1,24 @@
 #!/bin/bash
 
 restoref() {
+    COMMAND='git diff --name-only'
+    QUESTION_FLAG=true
+    OPTION=''
+
+    while getopts "s" opt; do
+        case ${opt} in
+            s )
+            COMMAND='git diff --name-only --cached '
+            QUESTION_FLAG=false
+            OPTION='--staged'
+            ;;
+        \? )
+            echo "Usage: $0 [-s]"
+            exit 1
+            ;;
+        esac
+    done
+
     # コマンド履歴を取得してfzfでフィルタリング
     cd $(git rev-parse --show-toplevel)
     select_file=$( git diff --name-only | gfzf --no-sort --prompt='SELECT RESTORE FILE: ' --multi)
@@ -12,32 +30,34 @@ restoref() {
         return 1
     fi
 
-    # ユーザーに質問をして y/n の回答を待つ
-    echo -n "restore all ok? (y/n) "
-    read  answer
+    if [ "$flag" = true ]; then
+        # ユーザーに質問をして y/n の回答を待つ
+        echo -n "restore all ok? (y/n) "
+        read  answer
 
-    # 回答を小文字に変換
-    answer=$(echo "$answer" | tr '[:upper:]' '[:lower:]')
+        # 回答を小文字に変換
+        answer=$(echo "$answer" | tr '[:upper:]' '[:lower:]')
 
-    # 回答によって処理を分岐
-    case "$answer" in
-        y|yes)
-            ;;
-        n|no)
-            cd -
-            echo "exit process"
-            return 1
-            ;;
-        *)
-            cd -
-            echo "unknown answer"
-            return 1
-            ;;
-    esac
+        # 回答によって処理を分岐
+        case "$answer" in
+            y|yes)
+                ;;
+            n|no)
+                cd -
+                echo "===== EXIT PROCESS ====="
+                return 1
+                ;;
+            *)
+                cd -
+                echo "===== UNKNOWN ANSWER ====="
+                return 1
+                ;;
+        esac
+    fi
 
 
     # 選択されたコマンドを実行
-    echo $select_file | xargs git restore
+    echo $select_file | xargs git restore $OPTION
     cd -
     echo "===== DONE ====="
 }
