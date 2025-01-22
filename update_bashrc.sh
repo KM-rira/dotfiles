e7d4908295ed1684111a80b997f2ae6d67cbe5e0
koji <>
2025-01-22 10:29:53 +0900

    rename add_alias.sh to aliases.sh

--- update_bashrc.sh
+++ update_bashrc.sh
@@ -20,11 +20,11 @@ add_line() {
 # 必要な行を.bashrcに追加
 add_line "source ~/dotfiles/pureline/pureline ~/.pureline.conf"
 add_line "# unalias -a"
-add_line "source ~/dotfiles/add_alias.sh"
+add_line "source ~/dotfiles/aliases.sh"
 add_line "HISTSIZE=100000"
 add_line "HISTTIMEFORMAT='%Y-%m-%d %H:%M:%S '"
 add_line "bash ~/dotfiles/pull_dotfiles.sh"
-add_line "source ~/dotfiles/add_alias.sh"
+add_line "source ~/dotfiles/aliases.sh"
 add_line 'export BAT_STYLE="header,grid"'
 
 echo "=== Updated $BASHRC if necessary. ==="
