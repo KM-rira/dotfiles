5086f84d7c11dfddc0df4fdaea900893d74ee93d
koji <>
2025-01-23 16:24:10 +0900

    add cheat command

--- installer/install_brew.sh
+++ installer/install_brew.sh
@@ -20,7 +20,7 @@ install_function() {
 
 # brew install
 install_cmd='brew install'
-packages=(git-delta dust tlrc glances glow yazi lazygit lazydocker)
+packages=(git-delta dust tlrc glances glow yazi lazygit lazydocker cheat)
 for package in "${packages[@]}"; do
 	install_function "$install_cmd" "$package"
 done
