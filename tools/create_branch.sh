40dfa73f5cbc508f88337ecdcd3f1d7635c20f38
Koji <74136309+KM-rira@users.noreply.github.com>
2025-02-16 13:49:46 +0900

    Update create_branch.sh

--- tools/create_branch.sh
+++ tools/create_branch.sh
@@ -1,6 +1,17 @@
 #!/bin/zsh
 
 createbranch() {
+	local local_only=0
+
+	# -l オプションが指定されているかチェック
+	while getopts "l" opt; do
+		case "$opt" in
+			l) local_only=1 ;;
+			*) echo "Usage: createbranch [-l] branch_name"; return 1 ;;
+		esac
+	done
+	shift $((OPTIND - 1))
+
 	if [ "$#" -eq 0 ]; then
 		echo "===== NEED PARAMETER ====="
 		return 1
@@ -8,5 +19,9 @@ createbranch() {
 
 	branch="$1"
 	git checkout -b "$branch"
-	git push -u origin "$branch"
+
+	# -l が指定されていない場合はリモートにプッシュ
+	if [ "$local_only" -eq 0 ]; then
+		git push -u origin "$branch"
+	fi
 }
