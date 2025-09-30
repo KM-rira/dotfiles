d898a361f020685c63735f3c93d4975781653b7f
koji <>
2025-10-01 01:15:49 +0900

    update installer

--- installer/normal_install.sh
+++ installer/normal_install.sh
@@ -30,10 +30,14 @@ install_function() {
 
 # brew install
 install_cmd='brew install'
-packages=(git-delta dust tlrc glances glow yazi lazygit lazydocker cheat usql bat luarocks zoxide jq fd-find ripgrep fzf zoxide shfmt)
+packages=(git-delta dust tlrc glances glow yazi lazygit lazydocker cheat usql bat luarocks zoxide jq fd-find ripgrep fzf zoxide shfmt lsd duf node npm chrony ccze vim)
+)
 for package in "${packages[@]}"; do
 	install_function "$install_cmd" "$package"
 done
+
+# 事後に実行
+# sudo chronyd -q 'server ntp.nict.jp iburst'
 
 # git-delta の設定
 git config --global core.pager delta
