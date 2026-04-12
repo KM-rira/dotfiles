6865a6e11c42a8051defe02bad20589337dc5c6a
Koji <>
2026-04-13 00:36:11 +0900

    add gemini model defalt

--- aliases.sh
+++ aliases.sh
@@ -189,7 +189,10 @@ alias continue='git rebase --continue'
 alias abort='git rebase --abort'
 alias amend='git commit --amend -m'
 alias rebasei='bash ~/dotfiles/tools/rebase_helper.sh'
+
+export GEMINI_MODEL=gemini-3-flash-preview
 alias ge='gemini'
+
 . ~/dotfiles/tools/commit.sh
 alias bmf="go run $dot/bookmark/book_mark_format.go;~/dotfiles/tools/book_mark_format/book_mark_fzf.sh"
 alias log='git log --oneline | nl | head -n 30'
